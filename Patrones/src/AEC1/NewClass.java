9688a198af63572a9845bcb37cc23e1f11aac24a
jfhs19 <jfhs19@usal.es>
2015-11-29 18:09:28 +0100

    AEC1 v0.01 Creacion paquete

---
+++ Patrones/src/AEC1/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AEC1;
+
+/**
+ *
+ * @author jfhs1
+ */
+public class NewClass {
+    
+}
